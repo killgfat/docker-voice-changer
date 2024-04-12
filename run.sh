f79e809fb4dd3c76bb11c636a6dcefe782c11e15
KGFAT <32797449+killgfat@users.noreply.github.com>
2024-04-12 13:52:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 socat TCP-LISTEN:18888,fork,bind=0.0.0.0 TCP:127.0.0.1:18887
-python3 MMVCServerSIO.py -p 18887 --https true \
+python3 MMVCServerSIO.py -p 18887 --https false \
     --content_vec_500 pretrain/checkpoint_best_legacy_500.pt  \
     --content_vec_500_onnx pretrain/content_vec_500.onnx \
     --content_vec_500_onnx_on true \
